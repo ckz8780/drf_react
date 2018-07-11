30fe61e0382346b162d7de0e4581f40ee9924ba0
Christopher Zielinski <ckz8780@gmail.com>
2018-07-11 00:08:24 -0400

    Styled index and removed erroneous code/files, updated readme

--- README.md
+++ README.md
@@ -9,4 +9,6 @@ This app tests the integration of Django REST Framework with a React front end.
 - Install requirements: `pip install -r requirements.txt`
 - Make migrations and migrate: `python manage.py makemigrations`, `python manage.py migrate`
 - Load dummy data: `python manage.py loaddata leads`
-- Run the dev server and navigate to http://127.0.0.1:8000
+- Run dev script and verify build is successful: `npm run dev`
+- Run the dev server: `python manage.py runserver`
+- Navigate to http://127.0.0.1:8000
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,22 +1,9 @@
 import React from "react";
-// import DataProvider from "./DataProvider";
-// import Table from "./Table";
-// import Form from "./Form";
-
 import ConnectedTable from "./ConnectedTable";
 import ConnectedForm from "./ConnectedForm";
 
 const App = () => (
 	<div>
-		{/*<React.Fragment>
-			<Form endpoint="api/lead/" />
-		</React.Fragment>
-
-		<React.Fragment>
-			<DataProvider endpoint="api/lead/" 
-			              render={data => <Table data={data} />} />
-		</React.Fragment>*/}
-
 		<React.Fragment>
 			<ConnectedForm endpoint="api/lead/" />
 		</React.Fragment>
